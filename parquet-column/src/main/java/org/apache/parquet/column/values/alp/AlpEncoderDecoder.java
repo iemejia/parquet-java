40e0a7e06fb0285ca61e4e910541a291eaad1b05
Ismaël Mejía <iemejia@gmail.com>
2026-05-23 06:28:58 +0200

    make spotkess happy

--- parquet-column/src/main/java/org/apache/parquet/column/values/alp/AlpEncoderDecoder.java
+++ parquet-column/src/main/java/org/apache/parquet/column/values/alp/AlpEncoderDecoder.java
@@ -233,8 +233,7 @@ final class AlpEncoderDecoder {
 
         int nonExceptions = length - exceptions;
         if (nonExceptions == 0) continue;
-        long delta = (nonExceptions < 2) ? 0 :
-            (long) maxEncoded - (long) minEncoded;
+        long delta = (nonExceptions < 2) ? 0 : (long) maxEncoded - (long) minEncoded;
         int bitsPerValue = (delta == 0) ? 0 : (64 - Long.numberOfLeadingZeros(delta));
         long estimatedSize = (long) length * bitsPerValue + (long) exceptions * (Float.SIZE + Short.SIZE);
         if (estimatedSize < bestEstimatedSize
@@ -296,8 +295,7 @@ final class AlpEncoderDecoder {
 
       int nonExceptions = length - exceptions;
       if (nonExceptions == 0) continue;
-      long delta = (nonExceptions < 2) ? 0 :
-          (long) maxEncoded - (long) minEncoded;
+      long delta = (nonExceptions < 2) ? 0 : (long) maxEncoded - (long) minEncoded;
       int bitsPerValue = (delta == 0) ? 0 : (64 - Long.numberOfLeadingZeros(delta));
       long estimatedSize = (long) length * bitsPerValue + (long) exceptions * (Float.SIZE + Short.SIZE);
       if (estimatedSize < bestEstimatedSize
